91a26db865107b0c44ea8659a361eb5df54672db
weijupeng <275526034@qq.com>
2020-01-09 18:10:01 +0800

    提交

--- src/main/java/com/design/mode/controller/ChannelController.java
+++ src/main/java/com/design/mode/controller/ChannelController.java
@@ -3,6 +3,7 @@ package com.design.mode.controller;
 import com.design.mode.service.channel.ChannelService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -12,12 +13,13 @@ import javax.annotation.Resource;
  * @date 2020/1/9 17:54
  */
 @RestController
+@RequestMapping("/channel")
 public class ChannelController {
 
     @Resource
     private ChannelService channelService;
 
-    @GetMapping("/channel/{code}")
+    @GetMapping("/{code}")
     public void channels(@PathVariable("code") String code) {
         channelService.channelFactorys(code);
     }
